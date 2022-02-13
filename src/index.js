b6129f00508eba97461e154ea46cd82f304ad9a6
denisselet <96264844+denisselet@users.noreply.github.com>
2022-02-13 19:49:54 +0200

    feat: task complited

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function toReadable (number) {
-  
-}
+    if (number === 0) {
+        return 'zero';
+    }
+    if (number === 1000) {
+        return "one thousand";
+    }
+    let arr = ["", "one", 'two', "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", ""];
+    let arr2 = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+     let c = [];
+    String(number).split('').reverse();
+    if (Math.floor(number / 100) > 0) {
+        c.push(arr[Math.floor(number / 100)]);
+        c.push('hundred');
+    }
+    if(number%100 < 21) {
+        if (number%100 !== 0) {
+            c.push(arr[number%100]);
+        }
+    }
+    if (number%100 > 20) {
+        c.push(arr2[Math.floor(number%100/10)]);
+        if((number%100)%10 !== 0) {
+            c.push(arr[(number%100)%10]);
+        }
+    }
+    return c.join(' ');
+};
